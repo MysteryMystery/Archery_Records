8e1a58d8a38a8b50515a71a3fc032577d59c4731
MysteryMystery <jamesattfield1990@gmail.com>
2017-03-28 20:47:45 +0100

    Add Member

--- src/main/scala/scene/form/mainmenu/MainMenu.scala
+++ src/main/scala/scene/form/mainmenu/MainMenu.scala
@@ -3,7 +3,10 @@ package scene.form.mainmenu
 import data.{ConfigLoader, DatabaseHandler}
 import util.{GUIUtil, ScriptingUtil}
 
+import scalafx.event.ActionEvent
+import scalafx.scene.control.Button
 import scalafx.scene.text.Text
+import ArcheryRecords._
 
 /**
   * Project: Archery_Records
@@ -12,4 +15,10 @@ import scalafx.scene.text.Text
   */
 class MainMenu extends GUIUtil with ScriptingUtil {
   override var sceneTitle: Text = new Text("Main Menu")
+
+  gridPane.add(new Button("Member"){
+    onAction = (event: javafx.event.ActionEvent) => {
+      logger.log(logger.INFO, this.getClass.getSimpleName, "To member button")
+    }
+  }, 1, 1)
 }
